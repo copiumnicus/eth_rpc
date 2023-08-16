5b3472d38fc4a762908f06426a49c4c6480ea9da
copiumnicus <copiumnicus@gmail.com>
2023-08-16 11:51:07 +0100

    allow no id in jr call

--- src/rpc_impl/jr_call.rs
+++ src/rpc_impl/jr_call.rs
@@ -6,6 +6,7 @@ use serde_json::Value;
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct JRCall {
     #[serde(with = "super::safe_id")]
+    #[serde(default)]
     pub id: u64,
     pub method: String,
     pub params: Value,
@@ -42,5 +43,19 @@ impl JRCall {
             method: method.to_string(),
             jsonrpc: "2.0".into(),
         })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_no_id() {
+        let call: JRCall = serde_json::from_str(
+            r#"{"jsonrpc": "2.0", "method": "eth_subscription", "params": {"subscription": "0xc581f553014f3d768aab1b45643fea51", "result": {"chainId": "0x1", "to": "0x709ccbd5c29e0f7bb2c315bb8c6b44dd2c6fc56e", "value": "0xe27c49886e60000", "data": "0x", "accessList": [], "nonce": "0x5f55f", "maxPriorityFeePerGas": "0x0", "maxFeePerGas": "0x5d21dba000", "gas": "0x33450", "type": "0x2", "hash": "0x0becb0e1d4fe219da225590e61475ba8a4f5bb285f45a721f1f7814648a6145c", "from": "0xbf94f0ac752c739f623c463b5210a7fb2cbb420b"}}}"#,
+        )
+        .unwrap();
+        assert_eq!(call.id, 0);
     }
 }
