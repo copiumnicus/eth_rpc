3799c20316936d0a3529af3d2306a2aaf2713d2d
copiumnicus <copiumnicus@gmail.com>
2023-09-08 12:50:21 +0100

    get pending block

--- src/rpc_impl/eth_get_block_by_number.rs
+++ src/rpc_impl/eth_get_block_by_number.rs
@@ -1,4 +1,4 @@
-use super::{JRCall, EthRpc, JRError};
+use super::{EthRpc, JRCall, JRError};
 use ethers::types::{Block, H256, U256};
 
 impl EthRpc {
@@ -6,6 +6,16 @@ impl EthRpc {
         let payload = JRCall::new(
             "eth_getBlockByNumber",
             vec![JRCall::to_value(number)?, JRCall::to_value(false)?],
+        )?;
+        self.no_ratelimit_rpc(payload)
+    }
+    pub fn get_pending_block(&self) -> Result<Block<H256>, JRError> {
+        let payload = JRCall::new(
+            "eth_getBlockByNumber",
+            vec![
+                JRCall::to_value(String::from("pending"))?,
+                JRCall::to_value(false)?,
+            ],
         )?;
         self.no_ratelimit_rpc(payload)
     }
@@ -20,5 +30,6 @@ mod test {
         let client = EthRpc::from_env().unwrap();
         let res = client.get_block_by_number(U256::from(17633288)).unwrap();
         assert_eq!(res.size, Some(U256::from(312244)));
+        client.get_pending_block().unwrap();
     }
 }
